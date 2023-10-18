cf26494b0d2d3ff93cf49baea965fe472f3a7bef
ShubhamTiwari479 <67504568+ShubhamTiwari479@users.noreply.github.com>
2023-10-18 11:30:07 +0530

    vyoma

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import AllRoutes from "./router/AllRoutes";
+import ScrollToTop from "./components/ScrollToTop";
+import AnimatedCursor from "react-animated-cursor";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="beny_tm_all_wrap">
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={44}
+        color="245, 34, 37"
+        outerAlpha={0.3}
+        innerScale={0.7}
+        outerScale={1.2}
+      />
+      <ScrollToTop />
+      <AllRoutes />
     </div>
   );
-}
+};
 
 export default App;
